d0e3fa1144c4fc399d37a3940403adfdd24f6426
Tom Verkon <tverkon@usa.net>
2022-04-11 11:20:18 -0400

    Second try

--- spring-reactive-api/src/main/java/com/baeldung/sra/api/foo/FooController.java
+++ spring-reactive-api/src/main/java/com/baeldung/sra/api/foo/FooController.java
@@ -1,16 +1,12 @@
 package com.baeldung.sra.api.foo;
 
 import java.time.Duration;
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.baeldung.sra.service.Foo;
-import com.baeldung.sra.service.FooService;
 
 import reactor.core.publisher.Flux;
 
@@ -18,13 +14,11 @@ import reactor.core.publisher.Flux;
 @RequestMapping("/api")
 public class FooController {
 
-	@Autowired
-	private FooService service;
 
-	@GetMapping(path = "/foo")
-	public Flux<Foo> getFlux(@RequestParam(required = true, defaultValue = "20") Integer count) {
-		List<Foo> foos = service.getFoo(count);
-		Flux<Foo> fluxFoos = Flux.fromIterable(foos).delayElements(Duration.ofSeconds(1)).log();
-		return fluxFoos;
-	}
+	@GetMapping(path = "/foo", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	  Flux<String> sseFlux() {
+	    return Flux.interval(Duration.ofSeconds(1))
+	        .map(aLong -> String.format("id:%d name:%s", aLong, RandomStringUtils.randomAlphabetic(7)));
+	  }	
+	
 }
--- spring-reactive-api/src/main/java/com/baeldung/sra/service/FooService.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.sra.service;
-
-import java.util.List;
-
-public interface FooService {
-	
-	List<Foo> getFoo(int fooCount);
-
-}
